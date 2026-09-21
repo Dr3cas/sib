a3f9d050522788d81825e85471cd378487523f57
Dr3cas <53199028+Dr3cas@users.noreply.github.com>
2026-09-21 11:25:03 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "André Gomes"
+__credits__ = ["André Gomes"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "André Gomes"
+__email__ = "andregomes.2508@gmail.com"
 
 
 
